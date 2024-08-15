eb5f04aff27bb552a95c25601223dc2c935a3239
TheKallman <142840599+TheKallman@users.noreply.github.com>
2024-08-15 14:49:39 -0500

    a

---
+++ Program2-1.cpp
@@ -0,0 +1,8 @@
+//program 2-1
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Programming is great fun!";
+}
---
+++ Program2-1/Program2-1/Program2-1.cpp
@@ -0,0 +1,14 @@
+//program 2-1
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << "Programming is great fun!" << endl;
+	cout << "Hello " << "there!\n";
+	cout << "Hello ";
+	cout << "there!" << endl;
+	cout << "test!" << endl;
+	cout << "Programming\nis\ngreat\nfun!";
+	cout << "\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a\a";
+}
