ee31c7851b95b14ed5d782b03ee40de427390e7a
TheKallman <142840599+TheKallman@users.noreply.github.com>
2024-08-26 14:49:18 -0500

    chapter 3 started

---
+++ Program3-1/Program3-1/Program3-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int length;
+	int width;
+
+	cout << "Input a value for length: ";
+	cin >> length;
+	cout << "Input a value for width: ";
+	cin >> width;
+}
