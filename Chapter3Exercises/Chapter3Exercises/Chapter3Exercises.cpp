1474f6ff16a646247b89e082ac26628a1bdcad88
TheKallman <142840599+TheKallman@users.noreply.github.com>
2024-09-05 14:50:28 -0500

    ehgdh

--- Chapter3Exercises/Chapter3Exercises/Chapter3Exercises.cpp
+++ Chapter3Exercises/Chapter3Exercises/Chapter3Exercises.cpp
@@ -4,34 +4,50 @@ using namespace std;
 int main()
 {
 	cout << "-----------------------------Rainfall---------------------------" << endl << endl;
-
-	//initialize variables
-	string month1, month2, month3;
-	float rainfall1, rainfall2, rainfall3, avgRain;
+	
+	//*initialize variables
+	//string month1, month2, month3;
+	//float rainfall1, rainfall2, rainfall3, avgRain;
 
 	//ask for the month and amount of rain in that month
-	cout << "Enter a month to record rainfall for: ";
-	cin >> month1;
-	cout << "Enter the rainfall for " << month1 << ": ";
-	cin >> rainfall1;
+	//cout << "Enter a month to record rainfall for: ";
+	//cin >> month1;
+	//cout << "Enter the rainfall for " << month1 << ": ";
+	//cin >> rainfall1;
 
-	cout << "Enter a month to record rainfall for: ";
-	cin >> month2;
-	cout << "Enter the rainfall for " << month2 << ": ";
-	cin >> rainfall2;
+	//cout << "Enter a month to record rainfall for: ";
+	//cin >> month2;
+	//cout << "Enter the rainfall for " << month2 << ": ";
+	//cin >> rainfall2;
 
-	cout << "Enter a month to record rainfall for: ";
-	cin >> month3;
-	cout << "Enter the rainfall for " << month3 << ": ";
-	cin >> rainfall3;
+	//cout << "Enter a month to record rainfall for: ";
+	//cin >> month3;
+	//cout << "Enter the rainfall for " << month3 << ": ";
+	//cin >> rainfall3;
 
 	//calculate and output average rainfall
-	avgRain = (rainfall1 + rainfall2 + rainfall3) / 3;
+	//avgRain = (rainfall1 + rainfall2 + rainfall3) / 3;
 	
-	cout << "The average rainfall for the months of " << month1 << ", " << month2
-		<< ", and " << month3 << " is: " << avgRain << " inches of rain.";
+	//cout << "The average rainfall for the months of " << month1 << ", " << month2
+	//	<< ", and " << month3 << " is: " << avgRain << " inches of rain.";
 
-	cout << "\n-----------------------------Cookies---------------------------" << endl << endl;
+	cout << "\n\n-----------------------------Cookies---------------------------" << endl << endl;
 
+	//make ingredients constants
+	double const sugar = 1.5;
+	double const butter = 1;
+	double const flour = 2.75;
 
+	//make input ingredients
+	double inSugar, inButter, inFlour;
+	int cookieAmount;
+
+	//ask for amount of cookies user wants to bake
+	cout << "Enter the number of cookies you want to bake: ";
+	cin >> cookieAmount;
+
+	//calculate the amount of ingredients needed for the amount of cookies wanted
+
+	double ingredientMult = cookieAmount % 48;
+	cout << ingredientMult;
 }
