6ff67a258bb9966ee0e1c9bc280acf94732b6dff
TheKallman <142840599+TheKallman@users.noreply.github.com>
2024-11-04 14:52:39 -0600

    cvxzzcxvvczzvc

--- CHAPTER6EXERCIESE/CHAPTER6EXERCIESE/CHAPTER6EXERCIESE.cpp
+++ CHAPTER6EXERCIESE/CHAPTER6EXERCIESE/CHAPTER6EXERCIESE.cpp
@@ -3,43 +3,84 @@
 
 #include <iostream>
 using namespace std;
+
 void exercise1();
 void exercise2();
 void exercise3();
 void exercise4();
-void exercise5();
+
 void quit();
 int main()
 {
-    std::cout << "Hello World!\n";
+    int choice = 0;
+    while (choice != 5)
+    {
+        do
+        {
+            cout << "\nWelcome to my Chapter 5 Exercises.\n";
+            cout << "Choose an exercise to run.\n";
+            cout << "1. Lowest Score Drop\n";
+            cout << "2. Overloaded Hospital\n";
+            cout << "3. Population\n";
+            cout << "4. Rock, Paper, Scissors, Lizard, Spock\n";
+            cout << "5. Quit\n";
+            cout << "Enter your selection: ";
+            cin >> choice;
+        } while (choice < 1 || choice > 5);
+
+        switch (choice)
+        {
+        case 1:
+        {
+            exercise1();
+            break;
+        }
+            
+        case 2:
+        {
+            exercise2();
+            break;
+        }
+        case 3:
+        {
+            exercise3();
+            break;
+        }
+        case 4:
+        {
+            exercise4();
+            break;
+        }
+        case 5:
+        {
+            cout << "Thank you for using whatever this is!!!! bye.";
+        }
+        }
+    }
 }
 
 void exercise1()
 {
-    cout << "a"
+    cout << "a";
 }
 
 void exercise2()
 {
-    cout << "b"
+    cout << "b";
 }
 
 void exercise3()
 {
-    cout << "c"
+    cout << "c";
 }
 
 void exercise4()
 {
-    cout << "d"
+    cout << "d";
 }
 
-void exercise5()
-{
-    cout << "e"
-}
 
 void quit()
 {
-    cout << "Hasta luego yung blud"
+    cout << "Hasta luego yung blud";
 }
