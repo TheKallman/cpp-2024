a976469e53f28b1e446dfd878e9ba6687f178c49
TheKallman <142840599+TheKallman@users.noreply.github.com>
2025-01-16 14:49:54 -0600

    hyiujopok

--- chapter7exericse/chapter7exericse/Exercise3 TTT.cpp
+++ chapter7exericse/chapter7exericse/Exercise3 TTT.cpp
@@ -9,36 +9,45 @@ void Exercise3()
 
 
 
-
-	//MAKE LABEL ON TICTACTOE BOARD FOR ROW AND COLUMN
-	//MAKE TURN SYSTEM
-
+	//FIX WINNING CONDITION THINGY
+	//FIX PUTTING SAME COORDINATE TWICE
 
 
-
+	int rowNum = 1;
 
 
 	// initalized borad
-	const char BOARD[3][3] = { {'*', '*', '*'},
-							   {'*', '*', '*'},
-							   {'*', '*', '*'} };
+	char BOARD[3][3] = { {'*', '*', '*'},
+						 {'*', '*', '*'},
+						 {'*', '*', '*'} };
 
 	// ask user if they want to play
 	char choice;
-	cout << "\nWould you like to play a game? (y/n)\n";
+	cout << "\nWould you like to play a game? (y/n) >> ";
 	cin >> choice;
+
+	cout << endl;
 
 	if (choice == 'y' || choice == 'Y')
 	{
 		// show board
+
+		//column numbers
+		cout << "       Column\n\t123\n";
 		for (int row = 0; row < 3; row++)
 		{
+			//row numbers
+			cout << "Row " << rowNum << ":  ";
+			rowNum++;
+
 			for (int col = 0; col < 3; col++)
 			{
 				cout << BOARD[row][col];
 			}
 			cout << endl;
 		}
+
+		cout << endl;
 
 		// start gaming
 		cout << "Player One goes first (X's)." << endl;
@@ -50,16 +59,49 @@ void Exercise3()
 		// initialize make coordiante place for players
 		int rowCoord;
 		int columnCoord;
+		char letter;
 
-		while (winStatus == false)
+		while (winStatus != true)
 		{
+			// reset row number to not make row number go to 29
+			rowNum = 1;
+
 			// change letter for each turn
-			if (p1Turn == 1 || p1Turn == 3 || p1Turn == 5 || p1Turn == 7 || p1Turn == 9)
-				char letter = 'X';
-			else char letter = 'O';
+			if (p1Turn == 1 || p1Turn == 3 || p1Turn == 5 || p1Turn == 7 || p1Turn == 9 || p1Turn == 11)
+				letter = 'X';
+			else letter = 'O';
 
-			cout << "Type your coordinates";
+			// GET SPOT TO PLACE X OR O
 
+			cout << "Enter your coordinates\n";
+			cout << "Type the row number: ";
+			cin >> rowCoord;
+
+			cout << "Type the column number: ";
+			cin >> columnCoord;
+
+			BOARD[rowCoord - 1][columnCoord - 1] = letter;
+
+			// PRINT BOARD
+
+			cout << endl;
+
+			//column numbers
+			cout << "       Column\n\t123\n";
+			for (int row = 0; row < 3; row++)
+			{
+				//row numbers
+				cout << "Row " << rowNum << ":  ";
+				rowNum++;
+
+				for (int col = 0; col < 3; col++)
+				{
+					cout << BOARD[row][col];
+				}
+				cout << endl;
+			}
+
+			cout << endl;
 
 
 			//CHECK WINS
@@ -79,7 +121,7 @@ void Exercise3()
 
 
 			// middle row win
-			if (BOARD[1][0] == BOARD[1][1] == BOARD[1][2] == 'X')
+			else if (BOARD[1][0] == BOARD[1][1] == BOARD[1][2] == 'X')
 			{
 				cout << "Player One wins!";
 				winStatus = true;
@@ -92,7 +134,7 @@ void Exercise3()
 			}
 
 			// bottom row win
-			if (BOARD[2][0] == BOARD[2][1] == BOARD[2][2] == 'X')
+			else if (BOARD[2][0] == BOARD[2][1] == BOARD[2][2] == 'X')
 			{
 				cout << "Player One wins!";
 				winStatus = true;
@@ -104,7 +146,7 @@ void Exercise3()
 			}
 
 			//  left column win
-			if (BOARD[0][0] == BOARD[1][0] == BOARD[2][0] == 'X')
+			else if (BOARD[0][0] == BOARD[1][0] == BOARD[2][0] == 'X')
 			{
 				cout << "Player One wins!";
 				winStatus = true;
@@ -116,7 +158,7 @@ void Exercise3()
 			}
 
 			// middle column win
-			if (BOARD[0][1] == BOARD[1][1] == BOARD[2][1] == 'X')
+			else if (BOARD[0][1] == BOARD[1][1] == BOARD[2][1] == 'X')
 			{
 				cout << "Player One wins!";
 				winStatus = true;
@@ -128,7 +170,7 @@ void Exercise3()
 			}
 
 			// right column win
-			if (BOARD[0][2] == BOARD[1][2] == BOARD[2][2] == 'X')
+			else if (BOARD[0][2] == BOARD[1][2] == BOARD[2][2] == 'X')
 			{
 				cout << "Player One wins!";
 				winStatus = true;
@@ -140,7 +182,7 @@ void Exercise3()
 			}
 
 			// left to right diagonal win
-			if (BOARD[0][0] == BOARD[1][1] == BOARD[2][2] == 'X')
+			else if (BOARD[0][0] == BOARD[1][1] == BOARD[2][2] == 'X')
 			{
 				cout << "Player One wins!";
 				winStatus = true;
@@ -152,7 +194,7 @@ void Exercise3()
 			}
 
 			// right to left diagonal win
-			if (BOARD[0][2] == BOARD[1][1] == BOARD[0][0] == 'X')
+			else if (BOARD[0][2] == BOARD[1][1] == BOARD[0][0] == 'X')
 			{
 				cout << "Player One wins!";
 				winStatus = true;
@@ -163,16 +205,12 @@ void Exercise3()
 				winStatus = true;
 			}
 
-
-
-
+			// change turn to next player
 			p1Turn++;
 		}
 		
 			
-		// change thing by matrix[row][col] = value;
-
-		// top row win
+		
 		
 
 	}
