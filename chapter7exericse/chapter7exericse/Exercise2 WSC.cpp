6010d96273148bafdccc054fd1ffaa9b82c1fde5
TheKallman <142840599+TheKallman@users.noreply.github.com>
2025-02-03 14:50:44 -0600

    km

--- chapter7exericse/chapter7exericse/Exercise2 WSC.cpp
+++ chapter7exericse/chapter7exericse/Exercise2 WSC.cpp
@@ -2,9 +2,18 @@
 #include <cstdlib>
 #include <string>
 #include <iomanip>
+#include <fstream>
 using namespace std;
 
 void Exercise2()
 {
-	cout << "hi";
+	ifstream Teams;
+	Teams.open("C:\\Users\\2012325\\Desktop\\cpp-2024\\datafiles\\Teams.txt");
+
+	if (!Teams) // make sure file exists
+		cout << "File not found.";
+	else // file exists, continue
+	{
+		cout << "File foind.";
+	}
 }
