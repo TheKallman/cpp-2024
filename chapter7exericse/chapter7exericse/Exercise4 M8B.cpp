67114f0d0e8ef30af621ca12a629958dc16b0768
TheKallman <142840599+TheKallman@users.noreply.github.com>
2025-03-07 14:51:41 -0600

    magic 8 ball done

--- chapter7exericse/chapter7exericse/Exercise4 M8B.cpp
+++ chapter7exericse/chapter7exericse/Exercise4 M8B.cpp
@@ -5,10 +5,13 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include <ctime>
 using namespace std;
 
 void Exercise4()
 {
+    unsigned seed = time(0);
+    srand(seed);
     //open file
     ifstream responses;
     responses.open("C:\\Users\\2012325\\Desktop\\cpp-2024\\datafiles\\8_ball_responses.txt");
@@ -33,23 +36,25 @@ void Exercise4()
     string again = "y";
     string question;
 
-    while (again == "y" || again == "Y")
+    while ((again == "y") || (again == "Y"))
     {
         //ask a question
         cout << "Ask a question for the mysterious and all-knowing Magic 7 Ball to answer.\n>> ";
+        
+        cin.ignore(INT_MAX, '\n');
         getline(cin, question);
-        cin.ignore();
-        cin.clear();
+        
         //make a random number to input as the index to create a random response
         //the - 1 and + 1 at the end might need to be changed to 0
-        int ranIndex = (rand() % (response.size() - 0 + 1)) + 0;
+        int ranIndex = (rand() % (response.size() - 1 + 1)) + 1;
 
         cout << response[ranIndex];
         
         cout << "\nWould you like to ask another question? (y/n)\n";
         
+        
         cin.clear();
-        cin.ignore(INT_MAX);
+
         cin >> again;
         
     }
