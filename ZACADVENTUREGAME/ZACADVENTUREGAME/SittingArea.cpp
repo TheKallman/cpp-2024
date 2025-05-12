2cd218fb11a93e1de0ce5f6673488c0b92b91294
TheKallman <142840599+TheKallman@users.noreply.github.com>
2025-05-12 14:14:42 -0500

    v

---
+++ ZACADVENTUREGAME/ZACADVENTUREGAME/SittingArea.cpp
@@ -0,0 +1,186 @@
+#include "Kitchen.h"
+#include "LaundryRoom.h"
+#include "Garage.h"
+#include "Backpack.h"
+#include "SittingArea.h"
+#include "Item.h"
+#include "Hallway.h"
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <cstdlib>
+#include <vector>
+#include <cmath>
+using namespace std;
+
+void SittingArea::onEnter()
+{
+    LaundryRoom laundryRoom;
+    SittingArea sittingArea;
+    Hallway hallway;
+    Garage garage;
+    Kitchen kitchen;
+    Item item;
+    Foyer foyer;
+    Backpack backpack;
+    string userInput;
+
+    cout << "\nYou are in the sitting area.";
+    cout << "\nWhat do you want to do?\n>> ";
+    getline(cin, userInput);
+
+    // Make input lowercase
+    for (char& c : userInput) c = tolower(c);
+    while (userInput != "quit")
+    {
+        //outputs for if the user asks to go a certain direction
+        if (userInput == "go east" || userInput == "east" || userInput == "walk east" || userInput == "move east")
+        {
+            bool hasBKey = false;
+            vector<string> items = backpack.getPockets();
+            for (size_t i = 0; i < items.size(); i++)
+            {
+                if (items[i] == "Blue Key")
+                {
+                    hasBKey = true;
+                }
+            }
+            if (hasBKey)
+            {
+                cout << "\nYou unlock the study with the blue key.\n";
+                backpack.useItem("Blue Key");
+                //study.onEnter() when exists
+            }
+            else
+            {
+                cout << "\nYou can see through the paned french doors that it is a study.\nYou try the blue knob, but notice it's locked.\n";
+            }
+        }
+        else if (userInput == "go north" || userInput == "north" || userInput == "walk north" || userInput == "move north")
+        {
+            kitchen.onEnter();
+        }
+        else if (userInput == "go south" || userInput == "south" || userInput == "walk south" || userInput == "move south")
+        {
+            hallway.onEnter();
+        }
+        else if (userInput == "go west" || userInput == "west" || userInput == "walk west" || userInput == "move west")
+        {
+            cout << "\nThere's a big window.\nNext to the big window are some chairs and a table.\n";
+        }
+
+        //outputs for if the user asks to look around hte room
+        else if (userInput == "look" || userInput == "look around" || userInput == "take a look" || userInput == "take a gander"
+            || userInput == "see" || userInput == "view" || userInput == "view room")
+        {
+            cout << "\nTo the north is the kitchen.\nTo the east is the study.\nTo the south is the hallway.\nTo the west is a window with chairs and a plant on a table.\n";
+        }
+
+        //output for if the user wants to look at the window or chairs or table or plant
+        else if (userInput == "look window" || userInput == "look at window" || userInput == "window" || userInput == "take a look at window"
+            || userInput == "view window" || userInput == "inspect window" || userInput == "look at the window" || userInput == "take a look at the window"
+            
+            || userInput == "look chairs" || userInput == "look at chairs" || userInput == "chairs" || userInput == "take a look at chairs"
+            || userInput == "view chairs" || userInput == "inspect chairs" || userInput == "look at the chairs" || userInput == "take a look at the chairs"
+            || userInput == "look chair" || userInput == "look at chair" || userInput == "chair" || userInput == "take a look at chair"
+            || userInput == "view chair" || userInput == "inspect chair" || userInput == "look at the chair" || userInput == "take a look at the chair"
+
+            || userInput == "sit" || userInput == "sit down" || userInput == "sit down on chair" || userInput == "sit on chair"
+            || userInput == "have a seat" || userInput == "take a seat" || userInput == "go take a seat" || userInput == "go have a seat"
+            
+            || userInput == "look table" || userInput == "look at table" || userInput == "table" || userInput == "take a look at table"
+            || userInput == "view table" || userInput == "inspect table" || userInput == "look at the table" || userInput == "take a look at the table"
+            
+            || userInput == "look plant" || userInput == "look at plant" || userInput == "plant" || userInput == "take a look at plant"
+            || userInput == "view plant" || userInput == "inspect plant" || userInput == "look at the plant" || userInput == "take a look at the plant")
+        {
+            cout << "\nYou sit down and take in the view outside.\nYou notice something off about the plant.\nYou search through it and find a green key.\n";
+            backpack.addItem("Green Key");
+        }
+
+        //output for if the user wants to look at their inventory
+        else if (userInput == "open inventory" || userInput == "open up the inventory" || userInput == "open the inventory" || userInput == "open up inventory"
+            || userInput == "look inventory" || userInput == "look at inventory" || userInput == "inventory" || userInput == "take a look at inventory"
+            || userInput == "view inventory" || userInput == "inspect inventory" || userInput == "look at the inventory" || userInput == "take a look at the inventory")
+        {
+
+            item.outputItems();
+        }
+
+        //output for if user inspects a certain item
+        else if (userInput == "look purple key" || userInput == "look at purple key" || userInput == "purple key" || userInput == "take a look at purple key"
+            || userInput == "take a look at the purple key" || userInput == "view purple key" || userInput == "inspect purple key")
+        {
+            item.inspectPKey();
+        }
+
+        else if (userInput == "look green key" || userInput == "look at green key" || userInput == "green key" || userInput == "take a look at green key"
+            || userInput == "take a look at the green key" || userInput == "view green key" || userInput == "inspect green key")
+        {
+            item.inspectGKey();
+        }
+
+        else if (userInput == "look shape code" || userInput == "look at shape code" || userInput == "shape code" || userInput == "take a look at shape code"
+            || userInput == "take a look at the shape code" || userInput == "view shape code" || userInput == "inspect shape code")
+        {
+            item.inspectDrawerComb();
+        }
+        else if (userInput == "look blue key" || userInput == "look at blue key" || userInput == "blue key" || userInput == "take a look at blue key"
+            || userInput == "take a look at the blue key" || userInput == "view blue key" || userInput == "inspect blue key")
+        {
+            item.inspectBKey();
+        }
+        else if (userInput == "look yellow key" || userInput == "look at yellow key" || userInput == "yellow key" || userInput == "take a look at yellow key"
+            || userInput == "take a look at the yellow key" || userInput == "view yellow key" || userInput == "inspect yellow key")
+        {
+            item.inspectYKey();
+        }
+        else if (userInput == "look oven's owner manual" || userInput == "look at oven's owner manual" || userInput == "oven's owner manual" || userInput == "take a look at oven's owner manual"
+            || userInput == "take a look at the oven's owner manual" || userInput == "view oven's owner manual" || userInput == "inspect oven's owner manual")
+        {
+            item.inspectOvenManual();
+        }
+        else if (userInput == "look screwdriver" || userInput == "look at screwdriver" || userInput == "screwdriver" || userInput == "take a look at screwdriver"
+            || userInput == "take a look at the screwdriver" || userInput == "view screwdriver" || userInput == "inspect screwdriver")
+        {
+            item.inspectScrew();
+        }
+        else if (userInput == "look soggy code" || userInput == "look at soggy code" || userInput == "soggy code" || userInput == "take a look at soggy code"
+            || userInput == "take a look at the soggy code" || userInput == "view soggy code" || userInput == "inspect soggy code")
+        {
+            item.inspectSogComb();
+        }
+        else if (userInput == "look washing machine owner's manual" || userInput == "look at washing machine owner's manual" || userInput == "washing machine owner's manual" || userInput == "take a look at washing machine owner's manual"
+            || userInput == "take a look at the washing machine owner's manual" || userInput == "view washing machine owner's manual" || userInput == "inspect washing machine owner's manual")
+        {
+            item.inspectWashManual();
+        }
+        else if (userInput == "look morse code" || userInput == "look at morse code" || userInput == "morse code" || userInput == "take a look at morse code"
+            || userInput == "take a look at the morse code" || userInput == "view morse code" || userInput == "inspect morse code")
+        {
+            item.inspectMorseComb();
+        }
+        else if (userInput == "look dumbbell" || userInput == "look at dumbbell" || userInput == "dumbbell" || userInput == "take a look at dumbbell"
+            || userInput == "take a look at the dumbbell" || userInput == "view dumbbell" || userInput == "inspect dumbbell"
+            || userInput == "look ten pound dumbbell" || userInput == "look at ten pound dumbbell" || userInput == "ten pound dumbbell" || userInput == "take a look at ten pound dumbbell"
+            || userInput == "take a look at the ten pound dumbbell" || userInput == "view ten pound dumbbell" || userInput == "inspect ten pound dumbbell")
+        {
+            item.inspectDumb();
+        }
+        else if (userInput == "look remote" || userInput == "look at remote" || userInput == "remote" || userInput == "take a look at remote"
+            || userInput == "take a look at the remote" || userInput == "view remote" || userInput == "inspect remote"
+            || userInput == "look tv remote" || userInput == "look at tv remote" || userInput == "tv remote" || userInput == "take a look at tv remote"
+            || userInput == "take a look at the tv remote" || userInput == "view tv remote" || userInput == "inspect tv remote")
+        {
+            item.inspectRemote();
+        }
+        else
+        {
+            cout << "\nI don't understand that command.\n";
+        }
+        cout << "\nWhat do you want to do?\n";
+        cout << ">> ";
+        getline(cin, userInput);
+    }
+
+}
